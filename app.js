31c80e92921e8eb4ba2984ab475f0335e8403ed7
Simon Wörnr <mail@simon-woerner.de>
2015-05-28 11:03:15 +0200

    added error handling

--- app.js
+++ app.js
@@ -1,67 +1,79 @@
+var domain = require('domain');
 var config = require('config');
 var ws = require('nodejs-websocket');
+var d = domain.create();
 
 var BSServer = require('./modules/BSServer.js');
 var BSUser = require('./modules/BSUser.js');
 var BSMessage = require('./modules/BSMessage.js');
 var Minibus = require('minibus');
 
+// error handling
+d.on('error', function(err) {
+    console.error(err);
+});
+
 // serve public folder
-var http_config = config.get('http');
-if(http_config !== false) {
-    var finalhandler = require('finalhandler');
-    var http = require('http');
-    var serveStatic = require('serve-static');
-    var serve = serveStatic('public', {'index': ['index.html']});
-    var http_server = http.createServer(function(req, res){
-        var done = finalhandler(req, res)
-        serve(req, res, done)
-    }).listen(http_config.port, http_config.hostname);
-}
+d.run(function() {
+    var http_config = config.get('http');
+    if(http_config !== false) {
+        var finalhandler = require('finalhandler');
+        var http = require('http');
+        var serveStatic = require('serve-static');
+        var serve = serveStatic('public', {'index': ['index.html']});
+        var http_server = http.createServer(function(req, res){
+            var done = finalhandler(req, res)
+            serve(req, res, done)
+        }).listen(http_config.port, http_config.hostname);
+    }
+});
 
-// bingo
-var bus = Minibus.create();
-var bingo = new BSServer(bus);
+// websocket server
+d.run(function() {
+    // bingo
+    var bus = Minibus.create();
+    var bingo = new BSServer(bus);
 
-// websocket
-var websocket_config = config.get('websocket');
-var server = ws.createServer(function (conn) {
-    console.log('# New connection');
-    bus.emit('connectionNew');
-    var user = null, sendEvent = null;
+    // websocket
+    var websocket_config = config.get('websocket');
+    var server = ws.createServer(function (conn) {
+        console.log('# New connection');
+        bus.emit('connectionNew');
+        var user = null, sendEvent = null;
 
-    conn.on('text', function (data) {
-        var msg = BSMessage.fromString(data);
+        conn.on('text', function (data) {
+            var msg = BSMessage.fromString(data);
 
-        console.dir(msg);
-        bingo.onMessage(msg);
-
-        if(msg.type === 'data' && msg.name === 'user') {
-            user = msg.data;
-            console.dir(user);
-            bingo.addPlayer(user);
-        }
-    });
-
-    conn.on('close', function (code, reason) {
-        console.log('# Connection closed: ' + code + ' - ' + reason);
-        if(user !== null) {
-            bingo.leaveGame(user.id);
-        }
-        bus.off(sendEvent);
-    });
-
-    sendEvent = bus.on('messageSend', function(msg){
-        if(msg.receiver === null || user === null || msg.receiver === user.id) {
-            try {
-                conn.sendText(msg.toString());
-            } catch(ex) {
-                console.log('could not send message.');
-                console.dir(ex);
-            }
             console.dir(msg);
-        }
-    });
+            bingo.onMessage(msg);
 
-    conn.sendText(new BSMessage('action', 'getUser', 'server', null, null).toString());
-}).listen(websocket_config.port, websocket_config.hostname);
+            if(msg.type === 'data' && msg.name === 'user') {
+                user = msg.data;
+                console.dir(user);
+                bingo.addPlayer(user);
+            }
+        });
+
+        conn.on('close', function (code, reason) {
+            console.log('# Connection closed: ' + code + ' - ' + reason);
+            if(user !== null) {
+                bingo.leaveGame(user.id);
+            }
+            bus.off(sendEvent);
+        });
+
+        sendEvent = bus.on('messageSend', function(msg){
+            if(msg.receiver === null || user === null || msg.receiver === user.id) {
+                try {
+                    conn.sendText(msg.toString());
+                } catch(ex) {
+                    console.log('could not send message.');
+                    console.dir(ex);
+                }
+                console.dir(msg);
+            }
+        });
+
+        conn.sendText(new BSMessage('action', 'getUser', 'server', null, null).toString());
+    }).listen(websocket_config.port, websocket_config.hostname);
+});
